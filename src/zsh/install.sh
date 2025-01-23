af9038e98e40a8f230ece8cb2528c2d3394fa4b6
Neil Enns <neile@live.com>
2025-01-23 07:05:20 -0800

    Add defaultUser option to zsh feature Fixes #14

--- src/zsh/install.sh
+++ src/zsh/install.sh
@@ -43,6 +43,9 @@ fi
 # set the theme
 upsert_config_option "^ZSH_THEME=.*$" "ZSH_THEME=\"$THEME\"" "$ZSH_RC_FILE"
 
+# set the default user
+upsert_config_option "^DEFAULT_USER=.*$" "DEFAULT_USER=\"$DEFAULTUSER\"" "$ZSH_RC_FILE"
+
 # configure the plugins
 upsert_config_option "^plugins=\\(.*\\)$" "plugins=($PLUGINS)" "$ZSH_RC_FILE"
 
